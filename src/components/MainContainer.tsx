9c50d8144d5a3acf2b76a6a05bffe81c7d0e6b10
yahalloe <lexuzherrera@gmail.com>
2025-10-11 15:29:33 +0800

    add border when hover and fix code to dynamically generate

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,73 +1,21 @@
 import { Link } from "react-router-dom";
+import { animeList } from "./animeData";
 
 export function MainContainer() {
   return (
-    // <div className="bg-[#1a1a1a] border border-stone-700 rounded-2xl shadow-lg p-5 w-full max-w-[1100px] mx-auto">
+    <div className="bg-black-111 w-full flex flex-col items-center justify-center text-stone-300 pt-5">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:gap-3 w-full max-w-[1000px] mx-auto p-2">
-        <Link to="anime/hyouka">
-          <img
-            title="Hyouka"
-            src="/hyouka.jpg"
-            className="object-cover w-full aspect-square rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/edgerunner">
-          <img
-            title="Edgerunner"
-            src="/edgerunner.gif"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/frieren">
-          <img
-            title="Frieren"
-            src="/fern.png"
-            className="object-cover w-full aspect- rounded-2xl w-[320px] h-[320px]"
-          />
-        </Link>
-        <Link to="anime/monogatari">
-          <img
-            title="Monogatari series"
-            src="/monogatari.jpg"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/suzumiya">
-          <img
-            title="Suzumiya Haruhi"
-            src="/suzumiya.jpg"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/madeInAbyss1">
-          <img
-            title="Made in Abyss"
-            src="/madeInAbyss1.jpg"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/hunter">
-          <img
-            title="Hunter x Hunter"
-            src="/hunterhunter.avif"
-            className="object-cover w-full aspect- rounded-2xl w-[320px] h-[320px]"
-          />
-        </Link>
-        <Link to="anime/bunnyGirl">
-          <img
-            title="Bunny Girl Senpai"
-            src="/bunnyGirl.jpg"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
-        <Link to="anime/86">
-          <img
-            title="86"
-            src="/86-eighty-six-lena-shin-4k--78.0_d.jpg"
-            className="object-cover w-full aspect- rounded-2xl"
-          />
-        </Link>
+        {animeList.map((anime) => (
+          <Link key={anime.id} to={`anime/${anime.id}`}>
+            <img
+              title={anime.title}
+              src={anime.image}
+              alt={anime.title}
+              className="object-cover w-full aspect-square rounded-2xl border-2 border-transparent hover:border-stone-100 transition-all duration-200"
+            />
+          </Link>
+        ))}
       </div>
-    // </div>
+    </div>
   );
 }
