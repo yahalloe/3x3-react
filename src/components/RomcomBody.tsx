9bd3f3bb5670226651dc0081bbce181822b7fd46
yahalloe <lexuzherrera@gmail.com>
2025-10-19 20:56:34 +0800

    add button in other 3x3's

--- src/components/RomcomBody.tsx
+++ src/components/RomcomBody.tsx
@@ -1,4 +1,5 @@
 import { romcomList } from "./animeData";
+import { BackButton } from "./BackButton";
 import { MainContainer } from "./MainContainer";
 
 export function RomcomBody() {
@@ -7,6 +8,9 @@ export function RomcomBody() {
         <MainContainer list={romcomList}/>
         <div className="flex flex-col max-w-[1000px] font-nunito items-center pt-30 pb-30 text-left text-zinc-300 sm:text-3xl md:text-4xl lg:text-3xl text-2xl px-5">
             These are peak shows in my peak watching experience.
+        </div>
+        <div className="min-w-5/6 flex justify-start pb-2 lg:min-w-1/2">
+            <BackButton />
         </div>
     </div>
   )
