98d262f123d89c38c348ef59e22b85f46568e531
yahalloe <lexuzherrera@gmail.com>
2025-10-20 10:11:41 +0800

    fix font on footer on mobile

--- src/components/AboutmeFooter.tsx
+++ src/components/AboutmeFooter.tsx
@@ -1,7 +1,7 @@
 export function AboutmeFooter() {
   return (
     <div className="black-111">
-      <div className="text-xl font-nunito flex justify-center pb-10 pt-10 text-zinc-700">
+      <div className="text-sm font-nunito flex justify-center pb-3 pt-3 text-zinc-700">
         © 2023-2025 YAHALLOE'S 3X3
       </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ export function Footer() {
   return (
     <div className="black-111">
       
-      <div className="text-lg lg:text-xl font-nunito flex justify-center pt-5 text-zinc-700 gap-5">
+      <div className="text-sm lg:text-xl font-nunito flex justify-center pt-3 text-zinc-700 gap-5">
         <Link to="/about" className="hover:text-stone-300">about me</Link>
         <a href="https://github.com/yahalloe/3x3-react" target="_blank" className="hover:text-stone-300">source</a>
         <a href="https://myanimelist.net/profile/yahalloe" target="_blank" className="hover:text-stone-300">MAL</a>
         <a href="https://animepicker.yahallo.tech" target="_blank" className="hover:text-stone-300">anime picker</a>
       </div>
-      <div className="text-lg lg:text-xl font-nunito flex justify-center pb-5 pt-3 text-zinc-700">
+      <div className="text-sm lg:text-xl font-nunito flex justify-center pb-3 pt-3 text-zinc-700">
         © 2023-2025 YAHALLOE'S 3X3
       </div>
     </div>
